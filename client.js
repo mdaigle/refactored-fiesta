eabf41af0ec532c673efc3a08c44e2664f8fde3d
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-04-07 22:27:40 -0700

    Adds file for protocol structure.

--- client.js
+++ client.js
@@ -1,15 +1,33 @@
+var protocol = require('./protocol');
+
 var args = process.argv.slice(2);
 
-const dgram = require('dgram');
-const socket = dgram.createSocket('udp4');
+const SESSIONID = protocol.makeSessionId();
+console.log("New session with id %d", SESSIONID);
+
+const DGRAM = require('dgram');
+const SOCKET = DGRAM.createSocket('udp4');
 
 //Send HELLO message to server
-socket.send(msg, args[1], args[0], (err) => {
-	socket.close();
-})
+/*SOCKET.send(msg, args[1], args[0], (err) => {
+	SOCKET.close();
+});*/
 
-socket.on('message', (msg, rinfo) => {
+SOCKET.on('message', (msg, rinfo) => {
 	console.log('Received %d bytes from %s:%d\n', 
 		msg.length, rinfo.address, rinfo.port);
 	//processmessage(msg, rinfo);
+});
+
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.on('line', (line) => {
+	//check connection state
+	//break apart if too long and send to server with header.
+});
+
+rl.on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
 });
---
+++ protocol.js
@@ -0,0 +1,34 @@
+// Protocol state constants.
+const HELLO = 0;
+const DATA = 1;
+const ALIVE = 2;
+const GOODBYE = 3;
+
+// Protocol header constants.
+const MAGIC = 0xC461;
+const VERSION = 1;
+
+module.exports.newMessage = newMessage;
+function newMessage(command_, sequence_number_, session_id_) {
+	var msg = {
+		magic: MAGIC,
+		version: VERSION,
+		command: command_,
+		sequence_number: sequence_number_,
+		session_id: session_id_
+	};
+	return msg;
+}
+
+// Encodes a p0p message with fields magic, version, command, sequence number, 
+// and session id.
+module.exports.messageEncode = messageEncode;
+function messageEncode(o) {
+
+}
+
+// Returns a random 32 bit unsigned int to use as a session id.
+module.exports.makeSessionId = makeSessionId;
+function makeSessionId() {
+	return Math.floor(Math.random() * (Math.pow(2, 32) - 1));
+}
